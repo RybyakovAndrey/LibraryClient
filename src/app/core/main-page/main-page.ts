3c739c0b9d0514ed0f3938a8bc17f13661a4de77
Rudger322 <vovannello@gmail.com>
2025-12-01 11:14:05 +0500

    feat: добавлен запрос данных на main-page

--- src/app/core/main-page/main-page.ts
+++ src/app/core/main-page/main-page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {BookDescription} from './components/book-description/book-description';
-import {BooksListPage} from './components/books-list-page/books-list-page';
+import {Book, BooksListPage} from './components/books-list-page/books-list-page';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButton} from '@angular/material/button';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-main-page',
@@ -17,6 +18,10 @@ import {MatButton} from '@angular/material/button';
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
-export class MainPage {
+export class MainPage implements OnInit {
+  httpClient = inject(HttpClient);
 
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:8000/getBooks').subscribe(res => {})
+  }
 }
